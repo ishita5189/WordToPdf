19bc46736c73df3438affed7346808529b77dfef
ishita5189 <ishita05sharma43@gmail.com>
2024-07-23 09:58:08 +0530

    Updated files

--- backend/index.js
+++ backend/index.js
@@ -8,29 +8,30 @@ const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({
+app.use(cors( {
     origin: '*',
-    methods: ['POST', 'GET'],
+    methods: ['POST' , 'GET'],
     credentials: true
-}));
+})
+);
+
 
 // Define a route for the root URL
 app.get("/", (req, res) => {
     res.send("Welcome to the File Conversion API");
 });
 
-// Setting up the file storage
+// settting up the file storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: function(req, file, cb) {
         cb(null, "uploads");
     },
-    filename: function (req, file, cb) {
+    filename: function(req, file, cb) {
         cb(null, file.originalname);
     },
 });
 
 const upload = multer({ storage: storage });
-
 app.post("/convertFile", upload.single("file"), (req, res, next) => {
     try {
         if (!req.file) {
@@ -42,7 +43,7 @@ app.post("/convertFile", upload.single("file"), (req, res, next) => {
         let outputPath = path.join(
             __dirname,
             "files",
-            `${path.parse(req.file.originalname).name}.pdf`
+            `${req.file.originalname}.pdf`
         );
         docxToPDF(req.file.path, outputPath, (err, result) => {
             if (err) {
